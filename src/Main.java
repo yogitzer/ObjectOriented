b2ffa05f70b909a9fad1c790fc3d2538e05f6f06
Mugyeong Kim <mugyeong92@gmail.com>
2025-09-17 12:48:36 +0900

    Homework1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            System.out.print("정수를 입력하세요: ");
+            int s = sc.nextInt();
+            sum += s;
+            System.out.println("현재까지 입력된 정수의 합은 "+sum+"입니다");
+        }
+    }
+}
